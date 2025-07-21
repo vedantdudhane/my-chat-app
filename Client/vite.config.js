87db1aafcaef2962448f8d3e4a2f9b3cc716e272
vedantdudhane <vedantdudhane4@gmail.com>
2025-07-21 11:04:50 +0530

    last

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+   resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [react(),tailwindcss(),],
 })
