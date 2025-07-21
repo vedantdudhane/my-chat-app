ea8eb0d867bffc2b53bda9754d4abb6b2598c80b
vedantdudhane <vedantdudhane4@gmail.com>
2025-07-21 10:38:55 +0530

    server updated for vercel

--- Server/server.js
+++ Server/server.js
@@ -49,7 +49,14 @@ app.use("/api/auth", userRouter)
 app.use("/api/messages", messageRouter)
 
 // Connect mongodb
+
 await connectDB();
+
+if(process.env.NODE_ENV !=='production'){
 const PORT = process.env.port || 5000;
 server.listen(PORT,()=> console.log("Server is Running on Port: "+ PORT));
+}
+
+// Exporting for vercel
+export default server;
 
